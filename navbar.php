882f3e2f152fa645399d9e477ffca413d9eccd23
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 01:27:25 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,9 +2,10 @@
 	include("header.php");
 ?>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
+            <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-top">
                 Home
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
