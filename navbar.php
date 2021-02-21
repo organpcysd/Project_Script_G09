7da43f5e6ebee5e1dfc0b484b0e515da92023fb0
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 00:22:53 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -5,17 +5,20 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                Navbar scroll
+                Home
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 <!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-            <form class="d-flex">
+            <form class="d-flex justify-content-end">
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">Log in</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sign in</a>
                     </li>
                 </ul>
             </div>
