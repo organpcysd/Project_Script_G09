033620d90aacc6b8fd1f215b2bbdd697032738e0
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 03:33:18 +0700

    Update navbar_login.php

--- navbar_login.php
+++ navbar_login.php
@@ -14,26 +14,17 @@
             <div class="collapse navbar-collapse" id="navbarScroll">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                        <a class="nav-link active" aria-current="page" href="#">My Post</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Manage Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Log out</a>
                     </li>
                 </ul>
             </div>
-<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-        <form class="d-flex">
-            <div class="dropdown">
-                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                     <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-            </div>
-        </form>
-<!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
         </div>
     </nav>
-
 </body>
 </html>
