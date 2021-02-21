c08c4964921566ef999525b5cc830498cff6f073
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 02:07:30 +0700

    Update navbar_login.php

--- navbar_login.php
+++ navbar_login.php
@@ -19,23 +19,18 @@
                 </ul>
             </div>
 <!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-            <form class="d-flex justify-content-end">
-            <div class="collapse navbar-collapse" id="navbarScroll">
-                    <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-                                <li><a class="dropdown-item" href="#">Create Post</a></li>
-                                <li><a class="dropdown-item" href="#">Edit Account</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Log out</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </form>
+        <form class="d-flex">
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                     <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </form>
 <!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
         </div>
     </nav>
