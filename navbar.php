8761dfd43715f4075fe0d6e6d9c4cb3050838732
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-21 23:08:28 +0700

    Update navbar.php

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<?php
+	include("header.php");
+?>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Navbar scroll
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+<!------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+            <form class="d-flex">
+            <div class="collapse navbar-collapse" id="navbarScroll">
+                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+            </form>
+<!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
+        </div>
+    </nav>
+
+</body>
+</html>
