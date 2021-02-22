c7be584024352c1fe98287d67ff280f4b3eceaaf
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 15:44:50 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -7,19 +7,13 @@
             <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-top">
                 Home
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
             <!-- Navbar Login,Register -->
-            <div class="collapse navbar-collapse" id="navbarScroll">
+            <form class="d-flex">
                 <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                    <li class="nav-item">
                         <a class="nav-link active" aria-current="page" id="login" href="#">Log in</a>
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link active" aria-current="page" id="register" href="#">Register</a>
-                    </li>
                 </ul>
+            </form>
             </div>
         </div>
     </nav>
