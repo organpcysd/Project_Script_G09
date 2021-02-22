b139ddfee162a82a2b62e91a153516bfafceee4c
organpcysd <b6121877@g.sut.ac.th>
2021-02-23 01:05:24 +0700

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -13,15 +13,7 @@
             $sql = "SELECT * FROM user WHERE username = '" . $username . "'";
             $rs = mysqli_query($con,$sql);
             $data = mysqli_fetch_array($rs);
-          
-
-            if ($data['user_type']==1) {
-                    include("navbar_user.php");
-            }elseif ($data['user_type']==2) {
-                    include("navbar_employee.php");
-            }elseif ($data['user_type']==3) {
-                    include("navbar_admin.php");
-        }
+            include("navbar_user.php");
         }
 	?>
 </head>
