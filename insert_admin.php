725cd248fa1bc0d901493e92ec6c99b61c817297
Tihitiwut <b6122324@g.sut.ac.th>
2021-02-22 19:50:07 +0700

    เพิ่มข้อมูล user

---
+++ insert_admin.php
@@ -0,0 +1,20 @@
+<?php
+include("db.php");
+
+$us = $_POST['username'];
+$fn = $_POST['fullname'];
+$ps = $_POST['password'];
+$em = $_POST['email'];
+$im = $_FILES['image']['name'];
+$sql1 = "ALTER TABLE user AUTO_INCREMENT = 1";
+$con->query("SET NAMES UTF8");    
+$sql="INSERT INTO user (fullname,username,password,email,image) values ('$us','$fn','$ps','$em','$im')";
+
+if ($con->query($sql) === true) {
+    move_uploaded_file($_FILES["image"]["tmp_name"], "images/user/" . $_FILES["image"]["name"]);
+    echo " Insertion Successfully!! ";
+} else {
+    echo "Error updating record: " . $con->error;
+}
+echo '<br> <a href="adminmanage.php"> Go to home </a></td>';
+?>
