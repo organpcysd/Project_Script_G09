03195decf4a0204b317d231f18db8d8d28a7f06c
COCODIZE <B6121891@g.sut.ac.th>
2021-02-22 19:52:53 +0700

    COCODIZE
    form

---
+++ insert.php
@@ -0,0 +1,31 @@
+<?php
+// connect to the database
+$con = mysqli_connect("play-hippy.net", "root", "Organ18032543","donatecenter");
+if(!$con) {
+    die("Could not connect: " . mysqli_error());
+  }else{
+  echo "";
+  }
+$con->query("SET NAMES UTF8");
+
+            $Topic = $_POST["topic"];
+            $Detail = $_POST["detail"];
+            $Address = $_POST["address"];
+            $Tel = $_POST['tel'];
+            $Email = $_POST["email"];
+            $Social = $_POST["social"];
+            $Images = $_POST["images"];
+
+
+
+$sql="INSERT INTO content (topic,datail,address,tel,email,social,images) values ('$Topic,$Detail,$Address,$Tel,$Email,$Social,$Images')";
+
+
+if ($con->query($sql) == TRUE) {
+    echo " Insertion Successfully!! ";
+} else {
+    echo "Error updating record: " . $con->error;
+}
+
+echo '<br> <a href="index.php"> Go to home </a></td>';
+?>
---
+++ insertForm.php
@@ -0,0 +1,50 @@
+<html>
+<?php
+  include("db.php");
+  include("header.php");
+?>
+
+<body>
+<form action="insert.php" method="post" autocomplete="on" enctype="multipart/form-data">
+
+<div class="container p-3 my-3 border">
+<legend>แบบฟอร์มเสนอมูลนิธิ สถานที่ รับบริจาค</legend>
+<b> ชื่อมูลนิธิ,สถานที่ ที่รับบริจาค</b><br>
+<input type="text" id="Topic"name="topic" >
+<br>
+    <b>รายละเอียดการรับบริจาค</b> <br>
+    <textarea id="Detail"  rows="4" cols="50" name="detail" >
+    </textarea>
+    <br>
+    <b>ข้อมูลการติดต่อ </b><br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+ • ที่อยู่ <br>
+<textarea id="Address"  rows="2" cols="50" name="address" > </textarea> <br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+• เบอร์โทร <br>
+<input type="text"id="Tel" name="tel" > <br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+• E-mail <br>
+<input type="email"id="Email" name="email" > <br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+• social <br>
+<input type="text" id="Social"name="social" > <br>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+• Picture 
+<br>
+<input type="file"id="Images" name="images"><br>
+<br>
+
+<input type="submit" class="btn btn-success" name="Save" value="Save">
+<input type="reset" class="btn btn-primary " value="ล้างข้อมูล"></TD></TR>
+
+</form>
+</Table>
+</body>
+</html>
+    
