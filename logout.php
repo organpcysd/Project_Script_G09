ef11bc74a7e4d62ee9792bc92271e8b50240cdfb
organpcysd <b6121877@g.sut.ac.th>
2021-02-22 17:18:56 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
 <?php 
 include("db.php");
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
 header("Location: index.php");
 session_destroy();
 ?>
-
-</form>
-</center>
-</body>
-</html>
