010b5e0129bc7043ee9b201874bc31a9ea276fe0
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-22 17:18:13 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
 ?>
     <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="index.php">
             Home
           </a>
          <!-- Navbar Login,Register -->
