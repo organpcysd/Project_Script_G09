9f67e2724e9bcf6cf8ca0b6ec03cee96d4388b55
BT_Chalis <B6123017@office365.sut.ac.th>
2021-02-23 01:03:19 +0700

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -48,7 +48,7 @@
                                         <td><img src="./images/avatar/avatar6.jpg" id="ava6" width="45" onclick="changeImg(6)"></td>
                                     </tr>
                                 </table>
-                            <h3 class="fieldset-title">Personal Info</h3>
+                            <h3 class="fieldset-title">ข้อมูลส่วนตัว</h3>
                             <div class="form-group avatar">
                                 <figure class="figure col-md-2 col-sm-3 col-xs-12">
                                     <img class="img-rounded img-responsive" src="images/user/<?php   echo "" .$data ["image"] ;?> " class="card-img-top" style="width: 150px">
@@ -58,7 +58,7 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">User Name</label>
+                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">ชื่อผู้ใช้</label>
                                 <fieldset disabled>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
                                     <input type="text" name = "username" class="form-control" value=<?php   echo "" .$data ["username"] ;?>>
@@ -67,33 +67,32 @@
                             </div>
         
                             <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Fullname</label>
+                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">ชื่อ - นามกลุล</label>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
                                     <input type="text" name = "fullname" class="form-control" value=<?php   echo "" .$data ["fullname"] ;?>>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Password</label>
+                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">รหัสผ่าน</label>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
                                     <input type="text" name = "password" class="form-control" value=<?php   echo "" .$data ["password"] ;?>>
                                 </div>
                             </div>
                         </fieldset>
                         <fieldset class="fieldset">
-                            <h3 class="fieldset-title">Contact Info</h3>
+                            <h3 class="fieldset-title">ช่องทางติดต่อ</h3>
                             <div class="form-group">
-                                <label class="col-md-2  col-sm-3 col-xs-12 control-label">Email</label>
+                                <label class="col-md-2  col-sm-3 col-xs-12 control-label">อีเมล</label>
                                 <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="email" name = "email"  class="form-control" value=<?php   echo "" .$data ["email"] ;?>>
-                                    <p class="help-block">This is the email </p>
+                                    <input type="email" name = "email"  class="form-control" value=<?php   echo "" .$data ["email"] ;?>>   
                                 </div>
                             </div>
                         </fieldset>
                         <hr>
                         <div class="form-group">
                             <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
-                                <input class="btn btn-primary" type="submit" value="Update Profile">
-                                <input class="btn btn-secondary" type = "submit" value="Cancle">
+                                <input class="btn btn-success" type="submit" value="บันทึก">
+                                <input class="btn btn-danger" type = "submit" value="ยกเลิก">
                             </div>
                         </div>
                     </form>
