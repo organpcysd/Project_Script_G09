2a8fb58b9fcbcfab859242772f1b1b9a826fe400
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-24 04:10:16 +0700

    Create showname.js

---
+++ showname.js
@@ -0,0 +1,22 @@
+var xmlHttp;
+function createXMLHttpRequest() {
+ if (window.ActiveXObject) // Internet Explorer
+ xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+ else // Firefox, Opera 8.0+, Safari
+ xmlHttp=new XMLHttpRequest();
+} //end function createXMLHttpRequest()
+function stateChange() {
+ if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete") {
+ // Add Code Here …….
+ document.getElementById("name").innerHTML = xmlHttp.responseText;
+ }
+} // end function statechange()
+function searchName(str)
+{
+ createXMLHttpRequest();
+ xmlHttp.onreadystatechange = stateChange;
+ var url = "adminmanage.php";
+ url = url + "?name=" + str;
+ xmlHttp.open("GET",url,true);
+ xmlHttp.send(null);
+} //end function showHint(str)
