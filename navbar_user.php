63d41530e04038e9861fb6750b27d577f7598505
BT_Chalis <B6123017@office365.sut.ac.th>
2021-02-24 00:26:20 +0700

    Update navbar_user.php

--- navbar_user.php
+++ navbar_user.php
@@ -39,11 +39,11 @@
                 });
 
                 $("#ava4").click(function(){
-                   $("body").css("backgroundColor","#1687a7");
+                   $("body").css("backgroundColor","#e57986");
                 });
 
                 $("#ava5").click(function(){
-                   $("body").css("backgroundColor","#c9cbff");
+                   $("body").css("backgroundColor","#e1ccec");
                 });
 
                 $("#ava6").click(function(){
