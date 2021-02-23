f20629287c897260c38703efac5d6803b8c56e9f
BT_Chalis <B6123017@office365.sut.ac.th>
2021-02-23 15:43:08 +0700

    Update navbar_user.php

--- navbar_user.php
+++ navbar_user.php
@@ -8,12 +8,12 @@
           </a>
          <!-- Navbar Login,Register -->
           <div>
-            <img src="./images/avatar/avatar1.png" id="ava1" width="45" onclick="changeImg(1)">
-            <img src="./images/avatar/avatar2.png" id="ava2" width="45" onclick="changeImg(2)">
-            <img src="./images/avatar/avatar3.png" id="ava3" width="45" onclick="changeImg(3)">
-            <img src="./images/avatar/avatar4.png" id="ava4" width="45" onclick="changeImg(4)">
-            <img src="./images/avatar/avatar5.png" id="ava5" width="45" onclick="changeImg(5)">
-            <img src="./images/avatar/avatar6.png" id="ava6" width="45" onclick="changeImg(6)">
+            <img src="./images/avatar/1.png" id="ava1" width="45" onclick="changeImg(1)">
+            <img src="./images/avatar/2.png" id="ava2" width="45" onclick="changeImg(2)">
+            <img src="./images/avatar/3.png" id="ava3" width="45" onclick="changeImg(3)">
+            <img src="./images/avatar/4.png" id="ava4" width="45" onclick="changeImg(4)">
+            <img src="./images/avatar/5.png" id="ava5" width="45" onclick="changeImg(5)">
+            <img src="./images/avatar/6.png" id="ava6" width="45" onclick="changeImg(6)">
             <a href="edit_profile.php" class="btn btn-outline-primary btn-sm">จัดการบัญชี</a>
             <a href="logout.php" class="btn btn-danger btn-sm">ออกจากระบบ</a>
           </div>
@@ -34,7 +34,7 @@
                 });
 
                 $("#ava4").click(function(){
-                   $("body").css("backgroundColor","#8ac6d1");
+                   $("body").css("backgroundColor","#e57986");
                 });
 
                 $("#ava5").click(function(){
