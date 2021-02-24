ad11f7732caf5a5b7169280d75d39e6aaab2dcb6
BT_Chalis <B6123017@office365.sut.ac.th>
2021-02-24 12:46:59 +0700

    Create searchName.js

---
+++ searchName.js
@@ -0,0 +1,34 @@
+var xmlHttp;
+function createXMLHttpRequest() {
+ if (window.ActiveXObject) // Internet Explorer
+ xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+ else // Firefox, Opera 8.0+, Safari
+ xmlHttp=new XMLHttpRequest();
+} //end function createXMLHttpRequest()
+
+function stateChange() {
+ if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete") {
+    var txt = xmlHttp.responseText;
+    
+    var status = new Array();
+    status = txt.split(",");
+
+    // alert(txt);
+
+    var add = "";
+    for(var i=0; i < status.length; i++) {
+        add += '<option value="' + status[i] + '" />';
+    }
+
+    document.getElementById("name").innerHTML = add;
+ }
+} // end function statechange()
+
+function searchName(str) {
+ createXMLHttpRequest();
+ xmlHttp.onreadystatechange = stateChange;
+ var url = "post.php";
+ url = url + "?name=" + str;
+ xmlHttp.open("GET",url,true);
+ xmlHttp.send(null);
+} //end function showHint(str)
