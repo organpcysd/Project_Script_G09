d469c146dc1500a260a1fee0a8b4a4b6e3f314a5
mrpumpkinz <b6121723@g.sut.ac.th>
2021-02-24 15:21:59 +0700

    Update showname.js

--- showname.js
+++ showname.js
@@ -5,14 +5,26 @@ function createXMLHttpRequest() {
  else // Firefox, Opera 8.0+, Safari
  xmlHttp=new XMLHttpRequest();
 } //end function createXMLHttpRequest()
+
 function stateChange() {
  if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete") {
- // Add Code Here …….
- document.getElementById("name").innerHTML = xmlHttp.responseText;
+    var txt = xmlHttp.responseText;
+    
+    var status = new Array();
+    status = txt.split(",");
+
+    // alert(txt);
+
+    var add = "";
+    for(var i=0; i < status.length; i++) {
+        add += '<option value="' + status[i] + '" />';
+    }
+
+    document.getElementById("name").innerHTML = add;
  }
 } // end function statechange()
-function searchName(str)
-{
+
+function searchName(str) {
  createXMLHttpRequest();
  xmlHttp.onreadystatechange = stateChange;
  var url = "adminmanage.php";
